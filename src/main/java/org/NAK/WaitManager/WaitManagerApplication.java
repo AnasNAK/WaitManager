f74287ca79bdc0bed1052fe7e32267501c9e20d7
AnasNAK <anasc00ding@gmail.com>
2024-11-11 14:20:45 +0100

    steup project spring

---
+++ src/main/java/org/NAK/WaitManager/WaitManagerApplication.java
@@ -0,0 +1,13 @@
+package org.NAK.WaitManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitManagerApplication.class, args);
+	}
+
+}
